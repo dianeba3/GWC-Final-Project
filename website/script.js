8140887a083e5ad1ea865d44ace04db04ce069b7
dianeba3 <41382203+dianeba3@users.noreply.github.com>
2018-08-01 21:43:51 -0500

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,58 @@
+var NorthwesternUniversity = ["Midwest", "Suburban",	"Private",	"15,000+",	"50,000+"];
+var UniversityofChicago = ["Midwest",];
+var UniversityofIllinois = ["Midwest",];
+var UniversityofNotre Dame = ["Midwest",];
+var OhioNorthernUniversity = ["Midwest",];
+var WashingtonUniversityinStLouis = ["Midwest",];
+var OberlinCollege = ["Midwest",];
+var UniversityofMichigan = ["Midwest",];
+var GrinnellCollege = ["Midwest",];
+var IowaStateUniversity = ["Midwest",];
+
+
+
+
+
+
+
+var listOfChosenAttributes =[];
+listOfChosenAttributes.push("HEY");
+
+function checkQ(idToCheck, attributeName) {
+    document.getElementById(idToCheck).checked = true;
+    listOfChosenAttributes.push(attributeName);
+}
+
+function submitQ1(){
+  var x = document.getElementById("DIV1st&2ndQ");
+  if(document.getElementById("option1Q1").checked == true || document.getElementById("option2Q1").checked == true || document.getElementById("option3Q1").checked == true || document.getElementById("option4Q1").checked == true || document.getElementById("option5Q1").checked == true){
+      x.style.display = "inline";
+    }
+}
+
+function submitQ2(){
+  var y = document.getElementById("DIV2nd&3rdQ");
+  if(document.getElementById("option1Q2").checked == true || document.getElementById("option2Q2").checked == true || document.getElementById("option3Q2").checked == true){
+      y.style.display = "inline";
+    }
+}
+
+function submitQ3(){
+  var y = document.getElementById("DIV3rd&4thQ");
+  if(document.getElementById("option1Q3").checked == true || document.getElementById("option2Q3").checked == true){
+      y.style.display = "inline";
+    }
+}
+
+function submitQ4(){
+  var a = document.getElementById("DIV4th&5thQ");
+  if(document.getElementById("option1Q4").checked == true || document.getElementById("option2Q4").checked == true || document.getElementById("option3Q4").checked == true || document.getElementById("option4Q4").checked == true){
+      a.style.display = "inline";
+    }
+}
+
+function submitQ5(){
+  var b = document.getElementById("RESULTS");
+      b.style.display = "inline";
+  //document.write(listOfChosenAttributes);
+}
